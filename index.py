38c4a42df41f9d566bbed581e49e5124d47ae6a9
suprim78 <suprimlamichhane90@gmail.com>
2022-05-20 14:05:23 +0545

    please

---
+++ index.py
@@ -0,0 +1,6 @@
+result = lambda n1,n2,n3:n1+n2+n3
+print ("sum of three values :",result(10,20,25))
+
+li=[5,7,22,97,54,62,77,23,73,61]
+square_list = list(map(lambda x: x**2, li))
+print(square_list)
